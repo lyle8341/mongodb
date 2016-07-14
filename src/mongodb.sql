97131e320a52afe5824f3a67c04107ea85e97f87
lyle <867708794@qq.com>
2016-07-14 17:33:01 +0800

    新增

---
+++ src/mongodb.sql
@@ -0,0 +1,24 @@
+/*
+ *  测试数据
+ *  数据是乱搞的。
+*/
+
+db.fuck.insert({"_id":1,"name":"商纣王","country":"商","age":50, "address":{"province":"河南","city":"洛阳"},"lover":["妲己","王后"]})
+db.fuck.insert({"_id":2,"name":"周幽王","country":"周","age":45, "address":{"province":"陕西","city":"岐山"}, "lover":["褒姒","文姜"]})
+db.fuck.insert({"_id":3,"name":"郑庄公","country":"郑","age":60, "address":{"province":"河南","city":"新郑"}, "lover":["祭足","颖考叔"]})
+db.fuck.insert({"_id":4,"name":"齐桓公","country":"齐","age":70, "address":{"province":"山东","city":"临淄"}, "lover":["易牙","竖刁"]})
+db.fuck.insert({"_id":5,"name":"宋襄公","country":"宋","age":45, "address":{"province":"江苏","city":"宋城"}, "lover":["目夷","司马"]})
+db.fuck.insert({"_id":6,"name":"秦穆公","country":"秦","age":59, "address":{"province":"陕西","city":"咸阳"}, "lover":["百里奚","伯姬"]})
+db.fuck.insert({"_id":7,"name":"晋文公","country":"晋","age":75, "address":{"province":"山西","city":"晋阳"}, "lover":["狐突","介子推"]})
+db.fuck.insert({"_id":8,"name":"秦始皇","country":"秦","age":60, "address":{"province":"陕西","city":"咸阳"}, "lover":["蒙恬","扶苏"]})
+db.fuck.insert({"_id":9,"name":"汉武帝","country":"汉","age":75, "address":{"province":"陕西","city":"长安"}, "lover":["霍去病","卫青"]})
+db.fuck.insert({"_id":10,"name":"魏武帝","country":"魏","age":68, "address":{"province":"河南","city":"许昌"}, "lover":["郭嘉","张辽"]})
+db.fuck.insert({"_id":11,"name":"唐太宗","country":"唐","age":64, "address":{"province":"陕西","city":"长安"}, "lover":["房玄龄","魏征"]})
+
+
+
+db.good.insert({"_id":1,"age":10,"height":10})
+db.good.insert({"_id":2,"age":20,"height":15})
+db.good.insert({"_id":3,"age":20,"height":175})
+db.good.insert({"_id":4,"age":20,"height":150})
+db.good.insert({"_id":5,"age":45,"height":170})
